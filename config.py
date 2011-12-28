f4d0e7b118a5969f26bc384453a74a23631bc49d
Matt Cooper <github.20.vtbassmatt@spamgourmet.com>
2011-12-28 06:42:21 -0800

    Added Backbone.js and dependencies

--- config.py
+++ config.py
@@ -9,5 +9,14 @@ class ConfigObj():
 Config = ConfigObj()
 Config.limits = ConfigObj()
 #Config.api = ConfigObj()
+Config.app = ConfigObj()
 
+# How many travelers will the system accept per trip?
 Config.limits.travelers_per_trip = 10
+
+# First API setting will go here
+#Config.api.<whatever> = <whatever>
+
+# Determines whether to append -min onto some JavaScript files or not
+# Should be True in production but can be moved to False for development
+Config.app.use_min = False
